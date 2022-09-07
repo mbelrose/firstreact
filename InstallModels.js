810d3fb75ab20eb5f47b43331e07fc0d5590e0bd
mbelrose <mike@belrose.net>
2022-09-06 23:03:19 -0500

    minor fix

--- InstallModels.js
+++ InstallModels.js
@@ -1,7 +1,7 @@
 //this script would normally be used to migrate schemas but mongoose doesn't do that.
 
 const mongoose = require('mongoose');
-const reviews = require('./model/ReviewSchema');
+const reviews = require('./model/ReviewModel');
 const ConnectionString = require('./model/ConnectionString');
 
 const logError = (error) => {
