757f63572a992d65b0559a5cc5bd06e6694c0c51
mbelrose <mike@belrose.net>
2022-09-11 23:57:19 -0500

    ibid

--- src/reviewLib/ReviewDetail.js
+++ src/reviewLib/ReviewDetail.js
@@ -1,22 +1,21 @@
 // given a userid, checks validity and displays rating details
 import React, { useState } from 'react';
-// import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import StarRating from './StarRating';
 import GenericError from '../GenericError';
 
 
 const ReviewDetail = () => {
 
-    // const {id} = useParams();
+    const {id} = useParams();
     const [rating, setRating] = useState(0);
     const [review, setReview] = useState({});
-    const id = '63182cc192b3f2ea552f01fd';
     fetch('/controller/review/' + id)
     .then((response) => { 
         return response.json();
-    }).then(( x ) => { 
-        setReview(x);
-        setRating(x.rating);
+    }).then(( reviewFetch ) => { 
+        setReview(reviewFetch);
+        setRating(reviewFetch.rating);
     }).catch(GenericError)
     .catch(() => { setReview({name: 'error', rating: 1});setRating(1) });
 
