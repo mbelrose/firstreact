da19c82971143ae1068ebd1c1a891b2b1f93922c
mbelrose <mike@belrose.net>
2022-09-13 21:12:23 -0500

    more cleanup

--- controller/ReviewController.js
+++ controller/ReviewController.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const ConnectionString = require('../model/ConnectionString');
-const reviewModel = require('../model/ReviewModel').model;
+const ReviewModel = require('../model/ReviewModel').model;
 const ControllerError = require('./ControllerError');
 
 const getOne =  (req, res, next) => {
@@ -14,7 +14,7 @@ const getOne =  (req, res, next) => {
         } catch (err){
             throw new Error('Bad review ID.');
         }
-        return reviewModel.findById(id);
+        return ReviewModel.findById(id);
 
     }).then( (review) => {
 
@@ -36,7 +36,7 @@ const getAll = (req, res, next) => {
     mongoose.connect(ConnectionString)
     .then(( connection ) => { 
 
-        return reviewsSearch = reviewModel.find();
+        return ReviewModel.find();
         
      }).then((reviews) => { 
         
