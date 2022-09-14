6e8249dc2152425ff15e626ff4a85af6491dc61d
mbelrose <mike@belrose.net>
2022-09-13 21:19:17 -0500

    ibid

--- controller/ReviewController.js
+++ controller/ReviewController.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const ConnectionString = require('../model/ConnectionString');
-const ReviewModel = require('../model/ReviewModel').model;
+const model = require('../model/ReviewModel').model;
 const ControllerError = require('./ControllerError');
 
 const getOne =  (req, res, next) => {
@@ -14,7 +14,7 @@ const getOne =  (req, res, next) => {
         } catch (err){
             throw new Error('Bad review ID.');
         }
-        return ReviewModel.findById(id);
+        return model.findById(id);
 
     }).then( (review) => {
 
@@ -36,7 +36,7 @@ const getAll = (req, res, next) => {
     mongoose.connect(ConnectionString)
     .then(( connection ) => { 
 
-        return ReviewModel.find();
+        return model.find();
         
      }).then((reviews) => { 
         
