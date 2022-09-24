73b63fb9883a571e621dff370e843a034cf944c7
mbelrose <mike@belrose.net>
2022-09-23 23:19:03 -0500

    change list to use StatusMessage

--- src/reviewLib/ReviewList.js
+++ src/reviewLib/ReviewList.js
@@ -1,13 +1,16 @@
-import React, {useState, useEffect } from "react";
+import React, {useState, useEffect, useRef } from "react";
 import { Link, useSearchParams } from 'react-router-dom';
+import StatusMessage from '../common/StatusMessage';
 
 
 export default function ReviewList () { 
 
-    const [errorMessage, setErrorMessage] = useState('_NONE');
-    const [loading, setLoading] = useState(true);
     const [reviews, setReviews] = useState([]);
     const [searchParams, setSearchParams] = useSearchParams();
+
+    const [status, setStatus] = useState({ type: 'IDLE' });
+    const statusClear = useRef(null);
+
 
     const [pages, setPages] = useState(1)
     const [page, setPage] = useState(
@@ -17,54 +20,52 @@ export default function ReviewList () {
 
     useEffect(() => { 
 
+        setStatus({ type: 'LOADING' });
         fetch('/controller/reviews/?page=' + page)
-        .then((response) => { 
-            return response.json();
-        }).then( (res ) => { 
-
-            setReviews(res.reviews);
-            setPages(parseInt(res.count / res.PAGE_SIZE));
-            setLoading(false);
-            if (res._errorMessage !== undefined) {
-                throw new Error(res._errorMessage); 
-            }
+        .then((res) => {
             
+            res.json().then( (msg) => {
+
+                if (msg._errorMessage) {
+                    setStatus({ type: 'ERROR', errorMessage: msg._errorMessage });
+                } else {
+                    setReviews(msg.reviews);
+                    setPages(parseInt(msg.count / msg.PAGE_SIZE));
+                    setStatus({ type: 'IDLE' });
+                }
+
+            }).catch( (err) => {
+                setStatus({ type: 'ERROR', errorMessage: 'No response.' });
+            });
+
         }).catch((err) => { 
-            setErrorMessage(err.message);
+            setStatus({ type: 'ERROR', errorMessage: err.message });
         });
     },[page]);
 
-        
-    if (errorMessage !== '_NONE') {
-        return (
-            <div>Error: {errorMessage}</div>
-        );
-    } else if (loading) {
-        return (
-            <div>LOADING...</div>
-        );
-    } else {
-        return (
-            <div>
-                <ul>
-                    { reviews.map( (i,j) => <li key={j}>
-                        <Link to={'/reviews/'+i._id}>
-                            {i.name}
-                        </Link>
-                    </li> )}
-                </ul>
-                <Link 
-                    to={'/reviews/?page=' + ((page > 1)?page - 1:1) } 
-                    onClick={e=>{e.preventDefault;setPage((page > 1)?page - 1:1 )}}
-                >&lt;&lt;PREV</Link>&nbsp;
-                Page {page } of { pages } &nbsp;
-                <Link 
-                    to={'/reviews/?page=' + ((page < pages)?page +1:pages) }
-                    onClick={e=>{e.preventDefault;setPage((page < pages)?page +1:pages )}}
-                >NEXT&gt;&gt;</Link>
-                <br/>
-            </div>
-        );
-    }
-
+    return (
+        <div>
+            <ul>
+                { reviews.map( (i,j) => <li key={j}>
+                    <Link to={'/reviews/'+i._id}>
+                        {i.name}
+                    </Link>
+                </li> )}
+            </ul>
+            <Link 
+                to={'/reviews/?page=' + ((page > 1)?page - 1:1) } 
+                onClick={e=>{e.preventDefault;setPage((page > 1)?page - 1:1 )}}
+            >&lt;&lt;PREV</Link>&nbsp;
+            Page {page } of { pages } &nbsp;
+            <Link 
+                to={'/reviews/?page=' + ((page < pages)?page +1:pages) }
+                onClick={e=>{e.preventDefault;setPage((page < pages)?page +1:pages )}}
+            >NEXT&gt;&gt;</Link>
+            <br/>
+            <StatusMessage 
+                status={status} setStatus={setStatus} 
+                statusClear={statusClear} 
+            />
+        </div>
+    );
 }
