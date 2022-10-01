c97a04360e8904786b6d2e523d2f21477aff49c5
mbelrose <mike@belrose.net>
2022-09-30 19:03:06 -0500

    cleanup react default text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { createContext } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import ReviewDetail from './reviewLib/ReviewDetail';
 import ReviewList from './reviewLib/ReviewList';
@@ -19,8 +18,8 @@ export default function App () {
         <div className="App">
 
           <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
+            
+            <h2>Reviews System</h2>
           </div>
 
           <div>
