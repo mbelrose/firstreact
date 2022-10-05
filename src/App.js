c5fde12155c8086aa2ae466eba574c21993aa77d
mbelrose <mike@belrose.net>
2022-10-04 22:56:21 -0500

    add spacer

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ export default function App () {
           <div className="App-header">
             
             <h2>Reviews System</h2>
+            <div class="spacer"></div>
           </div>
 
           <div className="App-sidebar">
