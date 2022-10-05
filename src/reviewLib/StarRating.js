6fabc875837a1257d7d3014f83fdf580279dcb97
mbelrose <mike@belrose.net>
2022-10-04 20:45:24 -0500

    don't define function in loop, use a fr click text

--- src/reviewLib/StarRating.js
+++ src/reviewLib/StarRating.js
@@ -9,18 +9,28 @@ export default function StarRating ({review, setField, updateRating}) {
     const STAR_LIMIT = 5;
     const [starSet, setStarSet] = useState([]);
 
+    const starClick = (i) => (e) => {
+        e.preventDefault();
+        setField({rating: i});
+        updateRating(i);
+    }
+
     useEffect (()=> {
         
         let iSet = [];
         for (let i=1; i<=STAR_LIMIT; i++)  {
             iSet.push(
-                <FaStar key={i}
-                color={i<=(review.rating||1)?'red':'grey'} 
-                onClick={e=> {
-                    setField({rating: i});
-                    updateRating(i);
-                }}
-            />);
+                <a 
+                    key={i}
+                    href="#" 
+                    onClick={starClick(i)}
+                >
+                    <FaStar
+                        key={i}
+                        color={i<=(review.rating||1)?'red':'grey'} 
+                    />
+                </a>
+            );
         }
         setStarSet(iSet);
     }, [review.rating]);
